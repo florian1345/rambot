d4a5a7835c8bd4a1230306a4d5fc9c675192d125
florian1345 <florian1345@gmx.de>
2022-07-17 01:25:44 +0200

    Added gaussian and inv gaussian filter plugin

---
+++ plugin-filters/src/lib.rs
@@ -0,0 +1,97 @@
+mod kernel;
+mod util;
+
+use crate::kernel::KernelFilter;
+
+use rambot_api::{
+    AdapterResolver,
+    AudioSource,
+    AudioSourceListResolver,
+    AudioSourceResolver,
+    EffectResolver,
+    Plugin
+};
+
+use std::collections::HashMap;
+
+fn get_sigma(key_values: &HashMap<String, String>) -> Result<f32, String> {
+    key_values.get("sigma")
+        .ok_or_else(|| "Missing argument \"sigma\".".to_owned())
+        .and_then(|s|
+            s.parse().map_err(
+                |e| format!("Error parsing value for \"sigma\": {}.", e)))
+}
+
+struct GaussianEffectResolver;
+
+impl EffectResolver for GaussianEffectResolver {
+
+    fn name(&self) -> &str {
+        "gaussian"
+    }
+
+    fn unique(&self) -> bool {
+        false
+    }
+
+    fn resolve(&self, key_values: &HashMap<String, String>,
+            child: Box<dyn AudioSource + Send>)
+            -> Result<Box<dyn AudioSource + Send>, String> {
+        let sigma = get_sigma(key_values)?;
+        Ok(Box::new(KernelFilter::new(child, kernel::gaussian(sigma))))
+    }
+}
+
+struct InvGaussianEffectResolver;
+
+impl EffectResolver for InvGaussianEffectResolver {
+
+    fn name(&self) -> &str {
+        "inv_gaussian"
+    }
+
+    fn unique(&self) -> bool {
+        false
+    }
+
+    fn resolve(&self, key_values: &HashMap<String, String>,
+            child: Box<dyn AudioSource + Send>)
+            -> Result<Box<dyn AudioSource + Send>, String> {
+        let sigma = get_sigma(key_values)?;
+        Ok(Box::new(KernelFilter::new(child, kernel::inv_gaussian(sigma))))
+    }
+}
+
+struct FiltersPlugin;
+
+impl Plugin for FiltersPlugin {
+    fn load_plugin(&self) -> Result<(), String> {
+        Ok(())
+    }
+
+    fn audio_source_resolvers(&self) -> Vec<Box<dyn AudioSourceResolver>> {
+        Vec::new()
+    }
+
+    fn effect_resolvers(&self) -> Vec<Box<dyn EffectResolver>> {
+        vec![
+            Box::new(GaussianEffectResolver),
+            Box::new(InvGaussianEffectResolver)
+        ]
+    }
+
+    fn audio_source_list_resolvers(&self)
+            -> Vec<Box<dyn AudioSourceListResolver>> {
+        Vec::new()
+    }
+
+    fn adapter_resolvers(&self) -> Vec<Box<dyn AdapterResolver>> {
+        Vec::new()
+    }
+}
+
+fn make_filters_plugin() -> FiltersPlugin {
+    FiltersPlugin
+}
+
+rambot_api::export_plugin!(make_filters_plugin);
